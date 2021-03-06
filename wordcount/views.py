ef105d2d7f1eb264c58798139df3ced52dd8187d
Elyor1996 <elyor@bnpz.uz>
2021-03-06 13:34:25 +0500

    last

--- wordcount/views.py
+++ wordcount/views.py
@@ -5,4 +5,17 @@ def homepage(request):
     return render(request, 'home.html', {'hithere':'This is me' })
 
 def count(request):
-    return render(request, 'count.html')
+    fulltext = request.GET['fulltext']
+
+    wordlist = fulltext.split()
+    worddictionary = {}
+
+    for word in wordlist:
+        if word in worddictionary:
+
+            worddictionary[word] += 1
+        else:
+            worddictionary[word] = 1
+
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count':len(wordlist), 'worddictionary':worddictionary.items()})
